3c35bce7a443a31fe02e416d2127d33170a902a7
ffalor <35144141+ffalor@users.noreply.github.com>
2023-07-21 20:57:05 +0000

    regenerate `/archives/entities/archive-files/v1`

--- falcon/client/sample_uploads/sample_uploads_client.go
+++ falcon/client/sample_uploads/sample_uploads_client.go
@@ -163,8 +163,9 @@ func (a *Client) ArchiveListV1(params *ArchiveListV1Params, opts ...ClientOption
 		return success, nil
 	}
 	// unexpected success response
-	unexpectedSuccess := result.(*ArchiveListV1Default)
-	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for ArchiveListV1: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
 }
 
 /*
