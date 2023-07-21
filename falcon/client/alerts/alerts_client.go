f59bacac1964191ee7a2a1866c6a34731e8e4f30
ffalor <35144141+ffalor@users.noreply.github.com>
2023-07-21 20:42:31 +0000

    regenerate `/alerts/queries/alerts/v1`

--- falcon/client/alerts/alerts_client.go
+++ falcon/client/alerts/alerts_client.go
@@ -74,8 +74,9 @@ func (a *Client) GetQueriesAlertsV1(params *GetQueriesAlertsV1Params, opts ...Cl
 		return success, nil
 	}
 	// unexpected success response
-	unexpectedSuccess := result.(*GetQueriesAlertsV1Default)
-	return nil, runtime.NewAPIError("unexpected success response: content available as default response in error", unexpectedSuccess, unexpectedSuccess.Code())
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for GetQueriesAlertsV1: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
 }
 
 /*
